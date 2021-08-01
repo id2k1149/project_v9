3996d44f72de2c80a7afcc316e63a04acc853914
id2k1149 <id2k1149@gmail.com>
2021-08-01 16:46:54 -0400

    v05 UserRepository

--- src/main/java/org/id2k1149/project_v9/repository/UserRepository.java
+++ src/main/java/org/id2k1149/project_v9/repository/UserRepository.java
@@ -2,7 +2,11 @@ package org.id2k1149.project_v9.repository;
 
 import org.id2k1149.project_v9.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
