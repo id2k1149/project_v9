6384083f87b8df90dd4523eaeddb7fe76fab3e39
id2k1149 <id2k1149@gmail.com>
2021-08-01 16:47:32 -0400

    v05 UserRepository

--- src/main/java/org/id2k1149/project_v9/service/UserService.java
+++ src/main/java/org/id2k1149/project_v9/service/UserService.java
@@ -4,21 +4,25 @@ import org.id2k1149.project_v9.model.Role;
 import org.id2k1149.project_v9.model.User;
 import org.id2k1149.project_v9.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class UserService  {
 
+    private final static String NOT_FOUND = "user with name %s is not found";
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(UserRepository userRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
-
 
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
@@ -26,8 +30,8 @@ public class UserService  {
         userRepository.save(user);
     }
 
-
-    public User findByUsername(String username) {
+    public User findByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+
     }
 }
