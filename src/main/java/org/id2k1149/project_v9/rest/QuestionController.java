750227b92b0314d3568143ebf6422e068d91d04f
alfie <id2k1149@gmail.com>
2021-08-04 18:28:09 -0400

    v15 front

---
+++ src/main/java/org/id2k1149/project_v9/rest/QuestionController.java
@@ -0,0 +1,18 @@
+package org.id2k1149.project_v9.rest;
+
+import org.id2k1149.project_v9.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/questions")
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+}
---
+++ src/main/java/org/id2k1149/project_v9/service/QuestionService.java
@@ -0,0 +1,18 @@
+package org.id2k1149.project_v9.service;
+
+import org.id2k1149.project_v9.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+}
