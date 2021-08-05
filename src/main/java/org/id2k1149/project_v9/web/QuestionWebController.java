1e46b2e032cfc2aa722f43218d1cc910478273ae
id2k1149 <id2k1149@gmail.com>
2021-08-05 16:27:03 -0400

    v22 front

--- src/main/java/org/id2k1149/project_v9/web/QuestionWebController.java
+++ src/main/java/org/id2k1149/project_v9/web/QuestionWebController.java
@@ -1,10 +1,13 @@
 package org.id2k1149.project_v9.web;
 
+import org.id2k1149.project_v9.model.Question;
 import org.id2k1149.project_v9.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class QuestionWebController {
@@ -18,7 +21,15 @@ public class QuestionWebController {
 
     @GetMapping({"/questions"})
     public String questions(Model model) {
+        List<Question> questionsList = questionRepository.findAll();
+        model.addAttribute("questionsList", questionsList);
         return "WEB-INF/jsp/questions";
+    }
+
+    @GetMapping({"/questions/new"})
+    public String newQuestionForm(Model model) {
+
+        return "WEB-INF/jsp/question_form";
     }
 
 
---
+++ src/main/webapp/WEB-INF/jsp/fragments/footer.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:set var="contextPath" value="${pageContext.request.contextPath}"/>
+
+<%--https://getbootstrap.com/docs/4.0/examples/sticky-footer/--%>
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted">sticky-footer</span>
+    </div>
+</footer>
+
