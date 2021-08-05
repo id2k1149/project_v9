dea8950afe8e9ac2ed212bc1bb782bbb74770d78
alfie <id2k1149@gmail.com>
2021-08-05 13:05:53 -0400

    v21 AnswerQuestionTests

--- src/main/java/org/id2k1149/project_v9/repository/AnswerRepository.java
+++ src/main/java/org/id2k1149/project_v9/repository/AnswerRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Repository
 @Transactional(readOnly = true)
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
     Answer findAnswerById(Long id);
-//    List<Answer> findAnswerByAnswerTitle(String answerTitle);
 }
