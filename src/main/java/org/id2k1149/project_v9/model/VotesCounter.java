5c8b5356550411ef8ef23db8e771dafd998ea25f
alfie <id2k1149@gmail.com>
2021-08-04 21:09:41 -0400

    v18 Voter VotesCounter

--- src/main/java/org/id2k1149/project_v9/model/VotesCounter.java
+++ src/main/java/org/id2k1149/project_v9/model/VotesCounter.java
@@ -18,4 +18,35 @@ public class VotesCounter {
 
     private Integer votes;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public Integer getVotes() {
+        return votes;
+    }
+
+    public void setVotes(Integer votes) {
+        this.votes = votes;
+    }
 }
