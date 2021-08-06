df12fe2bf158db4dbecb7a4a1a7dc6b02075ae35
id2k1149 <id2k1149@gmail.com>
2021-08-06 11:15:43 -0400

    v27 Desciption

---
+++ src/main/java/org/id2k1149/project_v9/service/DescriptionService.java
@@ -0,0 +1,31 @@
+package org.id2k1149.project_v9.service;
+
+import org.id2k1149.project_v9.model.Answer;
+import org.id2k1149.project_v9.repository.DescriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+@Service
+@Transactional
+public class DescriptionService {
+
+    private final DescriptionRepository descriptionRepository;
+
+    @Autowired
+    public DescriptionService(DescriptionRepository descriptionRepository) {
+        this.descriptionRepository = descriptionRepository;
+    }
+
+    public Map<Answer, DecimalFormat> makeMap() {
+        Map<Answer, DecimalFormat> newMap = new TreeMap<>();
+
+
+        return null;
+    }
+}
