c21be25b3e4e04a0e334e07e9267b722e691c0f1
id2k1149 <id2k1149@gmail.com>
2021-08-07 12:56:39 -0400

    v31 confirmation

--- src/main/java/org/id2k1149/project_v9/repository/QuestionRepository.java
+++ src/main/java/org/id2k1149/project_v9/repository/QuestionRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 @Transactional(readOnly = true)
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Question findQuestionById(Long id);
+    List<Question> findByDatePublished(LocalDate date);
 }
