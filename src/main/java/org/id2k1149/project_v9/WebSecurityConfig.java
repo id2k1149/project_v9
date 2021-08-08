1b1dd039f150b8166761443107ee95cc8f54976c
id2k1149 <id2k1149@gmail.com>
2021-08-08 18:04:48 -0400

    v44 login successUrl

--- src/main/java/org/id2k1149/project_v9/WebSecurityConfig.java
+++ src/main/java/org/id2k1149/project_v9/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
                 .loginPage("/login")
                 .permitAll()
+                .defaultSuccessUrl("/questions", true)
                 .and()
 
             .logout()
