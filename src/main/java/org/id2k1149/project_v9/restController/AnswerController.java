1fd89daf6b43346465b3e67fea4e82896bfe8040
id2k1149 <id2k1149@gmail.com>
2021-08-13 20:29:34 -0400

    v69 security

--- src/main/java/org/id2k1149/project_v9/restController/AnswerController.java
+++ src/main/java/org/id2k1149/project_v9/restController/AnswerController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/answers")
-@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+//@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public class AnswerController {
 
     private final AnswerService answerService;
--- src/main/java/org/id2k1149/project_v9/service/UserService.java
+++ src/main/java/org/id2k1149/project_v9/service/UserService.java
@@ -23,6 +23,7 @@ import java.util.Optional;
 
 @Service
 @Transactional
+
 public class UserService {
     private final UserRepository userRepository;
     private final VoterRepository voterRepository;
